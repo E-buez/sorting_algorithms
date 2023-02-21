f163cfeb9da3b59f8f34ad2c8730702e96160985
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-02-21 17:04:57 +0100

    Task 0

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef __SORT__
+#define __SORT__
 #include <stdlib.h>
 
 /**
@@ -17,11 +16,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/*======================================================================*/
+/*=================         Printing Algoritms        ==================*/
+/*======================================================================*/
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
+
+
+/*======================================================================*/
+/*=================          sorting Algoritms        ==================*/
+/*======================================================================*/
+
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
@@ -33,5 +40,10 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void swap_root(int *array, size_t root, size_t hi, size_t size);
 
-#endif
+#endif /* __SORT__ */
